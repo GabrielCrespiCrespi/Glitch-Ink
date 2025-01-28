c96db694742bb9d257a9567b29bceba4c8d1a76e
AdriaRebasa <adriarebasa@paucasesnovescifp.cat>
2025-01-28 12:49:46 +0100

    contador

--- script.js
+++ script.js
@@ -1,65 +1,45 @@
-iniciarContador('micontador');
+const expirationDate = 'May 28, 2025 23:59:59'; // Define or get your date
+    let deadlineTime = new Date(expirationDate);
 
-function iniciarContador(bloque){
-   let hoy = new Date();
-   let ini = hoy.valueOf();
-   let fin = leerFinal(bloque)
-   let tiempo = Math.floor((fin - ini)/1000); //pasar a segundos enteros)
-   let obContador = {timer:null, "tiempo":tiempo};
-   contar(obContador, bloque); //para evitar el delay inicial de 1 segundo
-   obContador.timer = setInterval(contar, 1000, obContador, bloque);
-   }
-//Decrementa el tiempo hasta la fecha final
-//Argumento:  objeto con  temporizador y el tiempo restante
- function contar(contador, bloque){
-   let partes = [];
-   if(contador.tiempo <= 0){
-      clearInterval(contador.timer);
-      contador.tiempo = 0;  //por si la fecha final ha pasado
-   }
-   else{
-      contador.tiempo--;            
-   }
-  partes = formatTiempo(contador.tiempo);
-  mostrarTiempo(partes, bloque);
-}
-//Convierte el tiempo restante en partes  dias, horas, minutos, segundos
-function formatTiempo(tiempo){ 
-    let dias, horas, minutos, segundos, partes;
-    [minutos, segundos] = dividir(tiempo, 60);
-    [horas, minutos] = dividir(minutos, 60);
-    [dias, horas] = dividir(horas, 24); 
-    partes = [dias, horas, minutos, segundos];
-    return partes;
- }
- //divide números enteros devuelve [cociente, resto]
- function dividir(num, den){
-    num = Math.floor(num);
-    let coc = Math.floor(num/den);
-    let resto = num % den;
-    return [coc, resto];
- }
- //Poner en la página web
-//Argumento : array dias,horas,minutos,segundos
-//Partes estarán dentro del bloque contador
-function mostrarTiempo(partes, bloque){
-    let bloques  = document.querySelectorAll("#"+bloque+" .dig span");
-    for(let i=0; i < bloques.length; i++){
-       bloques[i].innerHTML = partes[i].toString().padStart(2,0);
+    deadlineTime.setDate(deadlineTime.getDate());
+    let deadline = deadlineTime.getTime();
+    
+    // Function to update countdown timer
+    function updateCountdown() {
+      if (countdownInterval !== null) {
+        // Getting current time in required format
+        let now = new Date().getTime();    
+        let timeToLive = deadline - now;
+      
+        // Getting value of days, hours, minutes, seconds
+        let days = Math.floor(timeToLive / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((timeToLive % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((timeToLive % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((timeToLive % (1000 * 60)) / 1000);
+      
+        let daysElements = document.getElementsByClassName("days");
+        let hoursElements = document.getElementsByClassName("hours");
+        let minutesElements = document.getElementsByClassName("minutes");
+        let secondsElements = document.getElementsByClassName("seconds");
+    
+        // If you have many countdowns, you could fill all the tags using the classname
+        Array.from(daysElements).forEach(el => el.innerHTML = days);
+        Array.from(hoursElements).forEach(el => el.innerHTML = hours);
+        Array.from(minutesElements).forEach(el => el.innerHTML = minutes);
+        Array.from(secondsElements).forEach(el => el.innerHTML = seconds);
+      
+        // Output for over time
+        if (timeToLive < 0) {
+          countdownInterval && clearInterval(countdownInterval);
+          Array.from(daysElements).forEach(el => el.innerHTML = 0);
+          Array.from(hoursElements).forEach(el => el.innerHTML = 0);
+          Array.from(minutesElements).forEach(el => el.innerHTML = 0);
+          Array.from(secondsElements).forEach(el => el.innerHTML = 0);
+        }
+      }
     }
-  }
- //Lee el valor de  fecha final desde un elemento con id = contador
- //devuelve fecha final como valor
- //formato de fecha año-mes-dia o año/mes/dia
- function leerFinal(bloque){
-    let fecha;
-    let milisec = 0;
-    let datos = document.getElementById(bloque);
-    datos = datos.dataset.final;
-    fecha = new Date(datos);
-    if (!isNaN(fecha.valueOf())){
-       fecha = new Date(fecha);
-      milisec = fecha.valueOf();
-    }
-    return milisec;
-  }
+    
+    // To call defined function every second
+    let countdownInterval = null;
+    updateCountdown();    
+    countdownInterval = setInterval(updateCountdown, 1000);
