96ab746a52759123c4fe3eed1bf4256794d822c1
AdriaRebasa <adriarebasa@paucasesnovescifp.cat>
2025-01-30 11:35:40 +0100

    link du joc funcionant

--- script.js
+++ script.js
@@ -1,71 +1,69 @@
 const expirationDate = 'May 28, 2025 23:59:59'; // Define or get your date
-    let deadlineTime = new Date(expirationDate);
+let deadlineTime = new Date(expirationDate);
 
-    deadlineTime.setDate(deadlineTime.getDate());
-    let deadline = deadlineTime.getTime();
+deadlineTime.setDate(deadlineTime.getDate());
+let deadline = deadlineTime.getTime();
+
+// Function to update countdown timer
+function updateCountdown() {
+  if (countdownInterval !== null) {
+    // Getting current time in required format
+    let now = new Date().getTime();    
+    let timeToLive = deadline - now;
+      
+    // Getting value of days, hours, minutes, seconds
+    let days = Math.floor(timeToLive / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeToLive % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeToLive % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeToLive % (1000 * 60)) / 1000);
+      
+    let daysElements = document.getElementsByClassName("days");
+    let hoursElements = document.getElementsByClassName("hours");
+    let minutesElements = document.getElementsByClassName("minutes");
+    let secondsElements = document.getElementsByClassName("seconds");
     
-    // Function to update countdown timer
-    function updateCountdown() {
-      if (countdownInterval !== null) {
-        // Getting current time in required format
-        let now = new Date().getTime();    
-        let timeToLive = deadline - now;
+    // If you have many countdowns, you could fill all the tags using the classname
+    Array.from(daysElements).forEach(el => el.innerHTML = days);
+    Array.from(hoursElements).forEach(el => el.innerHTML = hours);
+    Array.from(minutesElements).forEach(el => el.innerHTML = minutes);
+    Array.from(secondsElements).forEach(el => el.innerHTML = seconds);
       
-        // Getting value of days, hours, minutes, seconds
-        let days = Math.floor(timeToLive / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((timeToLive % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((timeToLive % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((timeToLive % (1000 * 60)) / 1000);
-      
-        let daysElements = document.getElementsByClassName("days");
-        let hoursElements = document.getElementsByClassName("hours");
-        let minutesElements = document.getElementsByClassName("minutes");
-        let secondsElements = document.getElementsByClassName("seconds");
-    
-        // If you have many countdowns, you could fill all the tags using the classname
-        Array.from(daysElements).forEach(el => el.innerHTML = days);
-        Array.from(hoursElements).forEach(el => el.innerHTML = hours);
-        Array.from(minutesElements).forEach(el => el.innerHTML = minutes);
-        Array.from(secondsElements).forEach(el => el.innerHTML = seconds);
-      
-        // Output for over time
-        if (timeToLive < 0) {
-          countdownInterval && clearInterval(countdownInterval);
-          Array.from(daysElements).forEach(el => el.innerHTML = 0);
-          Array.from(hoursElements).forEach(el => el.innerHTML = 0);
-          Array.from(minutesElements).forEach(el => el.innerHTML = 0);
-          Array.from(secondsElements).forEach(el => el.innerHTML = 0);
-        }
-      }
+    // Output for over time
+    if (timeToLive < 0) {
+      countdownInterval && clearInterval(countdownInterval);
+      Array.from(daysElements).forEach(el => el.innerHTML = 0);
+      Array.from(hoursElements).forEach(el => el.innerHTML = 0);
+      Array.from(minutesElements).forEach(el => el.innerHTML = 0);
+      Array.from(secondsElements).forEach(el => el.innerHTML = 0);
     }
+  }
+}
     
-    // To call defined function every second
-    let countdownInterval = null;
-    updateCountdown();    
-    countdownInterval = setInterval(updateCountdown, 1000);
+// To call defined function every second
+let countdownInterval = null;
+updateCountdown();    
+countdownInterval = setInterval(updateCountdown, 1000);
 
-    const audioEpic = document.getElementById('audioEpic');
-    const muteButton = document.getElementById('muteButton');
+const audioEpic = document.getElementById('audioEpic');
+const muteButton = document.getElementById('muteButton');
 
-    muteButton.addEventListener('click', () => {
-        if (audioEpic.muted) {
-          audioEpic.muted = false;
-            muteButton.textContent = 'Deshabilitar Audio';
-        } else {
-            audioEpic.muted = true;
-            muteButton.textContent = 'Habilitar Audio';
-        }
-    });
+muteButton.addEventListener('click', () => {
+    if (audioEpic.muted) {
+      audioEpic.muted = false;
+        muteButton.textContent = 'Deshabilitar Audio';
+    } else {
+        audioEpic.muted = true;
+        muteButton.textContent = 'Habilitar Audio';
+    }
+});
 
+volumeControl.addEventListener('input', () => {
+  audioEpic.volume = volumeControl.value;
+});
 
-    volumeControl.addEventListener('input', () => {
-      audioEpic.volume = volumeControl.value;
-    });
-
-    const audio = document.getElementById('audio');
+const audio = document.getElementById('audio');
 const playButton = document.getElementById('play');
 const pauseButton = document.getElementById('pause');
-
 
 playButton.addEventListener('click', () => {
     audio.play();
@@ -78,3 +76,15 @@ pauseButton.addEventListener('click', () => {
 volumeControl.addEventListener('input', () => {
     audio.volumeControl = volumeControl.value;
 });
+
+function comprovant(){
+  let now = new Date().getTime();    
+  let timeToLive = deadline - now;
+
+  if(timeToLive>0){
+    alert("Beneit");
+  } else{
+    //Aqui anira u link du joc
+    alert("Maquina");
+  };
+};
