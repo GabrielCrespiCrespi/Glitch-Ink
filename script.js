1aa593a524b2f2f9a40a891969853a6570740ea9
AdriaRebasa <adriarebasa@paucasesnovescifp.cat>
2025-02-03 18:21:17 +0100

    boto de pujar cap adalt

--- script.js
+++ script.js
@@ -99,3 +99,35 @@ function scrollToElement(event, id) {
   event.preventDefault(); 
   document.getElementById(id).scrollIntoView({ block: "center" });
 }
+
+// const btnPujar = document.querySelector("#btCapAdalt");
+
+// btnPujar.addEventListener("click", function () {
+//   window.scrollTo({
+//     top: 0,
+//     left: 0,
+//     behavior: "smooth"
+//   });
+// });
+
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient( #12b481 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
