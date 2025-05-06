a09d7bd641abb49e9f4795200c10417a06f5b94c
AdriaRebasa <adriarebasa@paucasesnovescifp.cat>
2025-05-06 08:34:03 +0200

    tot du color que toca

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ let calcScrollValue = () => {
   scrollProgress.addEventListener("click", () => {
     document.documentElement.scrollTop = 0;
   });
-  scrollProgress.style.background = `conic-gradient( #8402a5 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+  scrollProgress.style.background = `conic-gradient(rgb(25, 24, 26) ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
 };
 
 window.onscroll = calcScrollValue;
